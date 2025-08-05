d9ddbd1bfd5e20068bc25d5b7276e3e2eafc46c5
Linconl Rufino <linconldasilvarufino@gmail.com>
2025-08-05 12:47:40 -0300

    configuring lower case urls

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
