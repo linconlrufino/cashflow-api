f63a0b95ac7cf38f04d5e7b85ae8dc34153bdc6f
Linconl Rufino <linconldasilvarufino@gmail.com>
2025-08-04 21:44:31 -0300

    implementing Validate on RegisterExpenseUseCase

--- Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,3 +1,4 @@
+using Communication.Enums;
 using Communication.Requests;
 
 namespace Application.UseCases.Expenses.Register;
@@ -6,6 +7,19 @@ public class RegisterExpenseUseCase
 {
     public RegisterExpenseRequest Execute(RegisterExpenseRequest request)
     {
+        Validate(request);
         return new RegisterExpenseRequest();
+    }
+
+    private void Validate(RegisterExpenseRequest request)
+    { 
+        if(string.IsNullOrWhiteSpace(request.Title))
+            throw new ArgumentException("Title is required.");
+        if(request.Amount <= 0)
+            throw new ArgumentException("Amount must be greater than zero.");
+        if (request.Date.CompareTo(DateTime.Today) > 0)
+            throw new ArgumentException("Expenses cannot be for the future.");
+        if(Enum.IsDefined(typeof(PaymentType), request.PaymentType))
+            throw new ArgumentException("Payment type is not valid.");
     }
 }
