1ceb3593df183d212b817fe6a49b64d8b17dbbd1
Linconl Rufino <linconldasilvarufino@gmail.com>
2025-09-02 18:29:03 -0300

    implementing RegisterExpenseValidator tests

--- Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,7 @@
 using Application.UseCases.Expenses.Register;
 using CommonTestUtilities.Requests;
+using Communication.Enums;
+using Exception;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -17,5 +19,66 @@ public class RegisterExpenseValidatorTests
         
         //Assert
         Assert.True(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Error_Title_Empty(string title)
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RegisterExpenseRequestBuilder.Build();
+        request.Title = title;
+
+        var result = validator.Validate(request);
+        
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, error => error.ErrorMessage.Equals(ResourcesErrorMessages.TITLE_REQUIRED));
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Amount_GreaterThan_Zero(decimal amount)
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RegisterExpenseRequestBuilder.Build();
+        request.Amount = amount;
+
+        var result = validator.Validate(request);
+        
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, error => error.ErrorMessage.Equals(ResourcesErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
+    
+    [Fact]
+    public void Error_Date_Cannot_Be_In_The_Future()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RegisterExpenseRequestBuilder.Build();
+        request.Date = DateTime.Today.AddDays(1);
+
+        var result = validator.Validate(request);
+        
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, error => error.ErrorMessage.Equals(ResourcesErrorMessages.EXPENSES_CANNOT_BE_FOR_THE_FUTURE));
+    }
+    
+    [Fact]
+    public void Error_PaymentType_Invalid()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RegisterExpenseRequestBuilder.Build();
+        request.PaymentType = (PaymentType)99;
+
+        var result = validator.Validate(request);
+        
+        Assert.False(result.IsValid);
+        Assert.Single(result.Errors);
+        Assert.Contains(result.Errors, error => error.ErrorMessage.Equals(ResourcesErrorMessages.PAYMENT_TYPE_IS_NOT_VALID));
     }
 }
